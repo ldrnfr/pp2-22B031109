b7c8a48be389d92ae2f2faf5db90f5c0751d30d7
ldrnfr <123300028+ldrnfr@users.noreply.github.com>
2023-01-29 22:38:29 +0600

    code

---
+++ TSIS 2/lists.py
@@ -0,0 +1,27 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1]) #1
+
+fruits = ["apple", "banana", "cherry"]
+fruits[0]= "kiwi" #2
+
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange") #3
+
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1,"lemon") #4
+
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana") #5
+
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1]) #6
+
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5]) #7
+
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits)) #8
+
+
+
+
